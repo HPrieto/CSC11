cbbe291268de78cfecd82d705b7e96e94f5b40d2
Heriberto Prieto <duskayame@gmail.com>
2017-09-14 00:03:05 -0700

    conversions assignment

---
+++ Class/convert_bases.cpp
@@ -0,0 +1,69 @@
+/*
+1 2 3(base 10) 	=> 100 + 20 + 3
+	  			=> 1 * 10^2 + 2 * 10^1 + 3 * 10^0
+	  			=> 123(base 10)
+
+1 2 3(base 8)	=> 100(base 8) + 20(base 8) + 3
+				=> 1 * 8^2 + 2 * 8^2 + 3 * 8^0
+				=> 83(base 10)
+
+1 2 3(base 16)  => 100(base 16) + 20(base 16) + 3
+				=> 1 * 16^2 + 2 * 16^1 + 3 * 16^0
+				=> 291(base 10)
+*/
+
+
+
+/*
+
+Homework Assignments
+
+101010(base 2) 	=> (base 10), (base 16), (base 8)
+
+456(base 8)		=> (base 10), (base 16), (base 2)
+
+ABC(base 16)	=> (base 8), (base 2), (base 10)
+
+117(base 10)	=> (base 8), (base 2), (base 16)
+
+*/
+
+/* Good and Bad of Flowcharting */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Hmwk/conversions.cpp
@@ -0,0 +1,60 @@
+//User Libraries
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+//Program Functions
+void convert(char *,int,int,int);
+void blnkRep(char *,int);
+void destroy(char *);
+char charRep(int);
+
+int main() {
+	//Declare program variables
+	int  bits  = 8, decimal, base;
+	char *cRep = new char[bits];
+	//Scan User Input
+	printf("Enter a number:");
+	scanf("%i", &decimal);
+	printf("Enter your desired base: ");
+	scanf("%i", &base);
+	//Perform Conversion
+	convert(cRep, bits,  decimal, base);
+	//Output representations to user
+	printf("String Representation: %s \n", cRep);
+	//Free Memory
+	destroy(cRep);
+}
+
+/* Converts int decimal value to specified base representation */
+void convert(char *cRep, int bits, int decimal, int base) {
+	//Conversion Variables
+  	int index = 1, remainder;
+  	//Set binary/octal/hex string representation to 0's
+  	blnkRep(cRep,bits);
+  	//Perform division algorithm
+  	while (decimal != 0) {
+    	remainder = decimal % base;
+    	decimal /= base;
+    	(remainder < 10) ? cRep[bits - index] = '0' + remainder:
+                       	   cRep[bits - index] = charRep(remainder);
+    	index++;
+  	}
+}
+
+/* Sets character representation to all zeros */
+void blnkRep(char *cRep,int bits) {
+  	for (int i = 0; i < bits; i++)
+    	cRep[i] = '0';
+  	cRep[bits] = '\0';
+}
+
+/* Frees memory in dynamic array */
+void destroy(char *array) {
+  	delete [] array;
+}
+
+/* Returns character representation of a 'number', including numbers > 9 */
+char charRep(int value) {
+  	return static_cast<char>('A' + (value - 10));
+}
